c0f16517e9cd61ed60e86e118ab02909f2323950
W0793048 <W0793048@myscc.ca>
2022-11-24 10:16:19 -0500

    added ISP_Call.cs...

---
+++ RiddhiBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,12 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiddhiBooks.DataAccess.Repository
+{
+    interface ISP_Call : IDisposable
+    {
+        T Single<T>(string procedurename, DynamicParameters)
+    }
+}
